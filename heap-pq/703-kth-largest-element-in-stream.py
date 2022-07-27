8320a575178f0e7e137b471189a31e8189b68658
Will Pine <wpine215@gmail.com>
2022-07-26 23:52:09 -0400

    Create 703-kth-largest-element-in-stream.py

---
+++ heap-pq/703-kth-largest-element-in-stream.py
@@ -0,0 +1,14 @@
+import bisect
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.list = sorted(nums)
+
+    def add(self, val: int) -> int:
+        bisect.insort(self.list, val)
+        return self.list[(-1 * self.k)]
